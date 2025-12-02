bc44ae051d45d9577106656964bb8b6207b54fb1
sesili <sesilianthonypeter@gmail.com>
2025-12-02 09:51:50 +0530

    prepared for deployement

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,6 @@ app.get("/", (req, res) => {
   res.send("Employee Attendance System API Running...");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+export default app;
+
 
