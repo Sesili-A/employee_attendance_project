7f7a2412e876a357bbd138489a232b38a04e7d85
sesili <sesilianthonypeter@gmail.com>
2025-12-02 10:13:49 +0530

    prepared for deployement

--- backend/server.js
+++ backend/server.js
@@ -6,27 +6,35 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
 import autoAbsentMiddleware from "./middlewares/autoAbsentMiddleware.js";
+
 dotenv.config();
 connectDB();
+
 const allowedOrigins = [
   process.env.CORS_ORIGIN,
   "http://localhost:3000",
-  
 ];
+
 const app = express();
+
 app.use(express.json());
 app.use(autoAbsentMiddleware);
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(morgan("dev"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
@@ -34,6 +42,7 @@ app.get("/", (req, res) => {
   res.send("Employee Attendance System API Running...");
 });
 
-export default app;
-
-
+// REQUIRED FOR VERCEL:
+export default function handler(req, res) {
+  return app(req, res);
+}
